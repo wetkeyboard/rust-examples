7ffbf3bbca50b2d48f69d746b983ce49c2086741
wetkeyboard17 <wetkeyboard17@gmail.com>
2022-11-03 00:55:47 +0000

    descriptions has been added into no_std_call_libc_functions.

--- no_std_call_libc_functions/src/main.rs
+++ no_std_call_libc_functions/src/main.rs
@@ -8,7 +8,9 @@ pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
     const HAYSTACK: & str = "Hello World!\n\0";
     const NEEDLE: & str = "World!\n\0";
     unsafe {
+        // Finds the first occurrence of a substring in a string
         let result = libc::strstr(HAYSTACK.as_ptr() as *const i8, NEEDLE.as_ptr() as *const i8);
+        //Prints characters and values to stdout.
         libc::printf(result);
     }
     0
