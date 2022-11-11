49ef1fa97b90ca066b19aeee18bc720a636874dc
wetkeyboard17 <wetkeyboard17@gmail.com>
2022-11-11 02:54:18 +0000

    Borrowing example 1.

--- borrwing/src/main.rs
+++ borrwing/src/main.rs
@@ -1,27 +1,13 @@
-// This function takes ownership of the heap allocated memory
-fn destroy_box(c: Box<i32>) {
-    println!("Destroying a box that contains {}", c);
-    // `c` is destroyed and the memory freed
-}
-
 fn main() {
-    // _Stack_ allocated integer
-    let x = 5u32;
-
-    // *Copy* `x` into `y` - no resources are moved
-    let y = x;
-
-    // Both values can be independently used
-    println!("x is {}, and y is {}", x, y);
-
-    // `a` is a pointer to a _heap_ allocated integer
-    let a = Box::new(5i32);
-
-    println!("a contains: {}", a);
-
-    // *Move* `a` into `b` after this point `a` does not exist anymore
-    let b = a;
-
-    destroy_box(b);
-
+    let y: &i32;
+    {
+        let x = 5;
+        y = &x;
+        println!("{}", y);
+    }
+    
+    // This println belove is not possible as `y` is only valid for the scope where `x` exists. So, this causes error:
+    // error: `x` does not live long enough
+    // y = &x;
+    //println!("{}", y);
 }
