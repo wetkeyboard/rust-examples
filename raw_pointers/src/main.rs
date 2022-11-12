aac5a3ba87b681357e96399355094d7e30211325
wetkeyboard17 <wetkeyboard17@gmail.com>
2022-11-12 05:35:08 +0000

    First version of raw pointers example.

--- raw_pointers/src/main.rs
+++ raw_pointers/src/main.rs
@@ -2,7 +2,8 @@ fn main() {
     // Creating a raw pointer is safe.
     let raw_p: *const u32 = &99;
 
-    // The raw pointers dereferencing is not checked by the compile-time checks process so they must be wrapped by unsafe{}.
+    // The raw pointers dereferencing is not checked by the compile-time checking process so they must be wrapped by unsafe{}.
+
     // This works, but it is not the best practice as the unsafe part in the code should be as short as it can be.
     unsafe {
         if *raw_p == 99 {
@@ -10,7 +11,7 @@ fn main() {
         }
     }
 
-    // So, this does the same, and it is a better practice.
+    // So, this does the same that code above, but this is a better practice.
     let p = unsafe {*raw_p};
     if p == 99 {
         println!("{}", p);
