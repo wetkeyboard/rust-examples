1c7f71fcbc4943da8e49401aac5e159a0757062e
wetkeyboard17 <wetkeyboard17@gmail.com>
2023-05-29 12:24:56 +0100

    Playing against omputer, basic version..

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::io;
+use rand::Rng;
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum Player {
@@ -7,6 +7,7 @@ enum Player {
     O,
 }
 
+// Implement the Display trait for Player enum to customize its string representation
 impl fmt::Display for Player {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -21,14 +22,17 @@ struct Board {
 }
 
 impl Board {
+    // Create a new instance of the board with empty cells
     fn new() -> Self {
         Board { cells: [None; 9] }
     }
 
+    // Make a move on the board at the specified position with the given player
     fn make_move(&mut self, position: usize, player: Player) {
         self.cells[position] = Some(player);
     }
 
+    // Display the current state of the board
     fn display(&self) {
         for (i, cell) in self.cells.iter().enumerate() {
             if let Some(player) = cell {
@@ -46,21 +50,29 @@ impl Board {
         }
     }
 
+    // Check if the specified player has won the game
     fn check_win(&self, player: Player) -> bool {
         let win_conditions = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6], // Diagonals
         ];
 
         win_conditions.iter().any(|&[a, b, c]| {
             self.cells[a] == Some(player) && self.cells[b] == Some(player) && self.cells[c] == Some(player)
         })
+    }
+
+    // Get a vector of available moves (empty positions) on the board
+    fn get_available_moves(&self) -> Vec<usize> {
+        self.cells.iter().enumerate()
+            .filter_map(|(index, &cell)| if cell.is_none() { Some(index) } else { None })
+            .collect()
+    }
+
+    // Check if the board is full
+    fn is_full(&self) -> bool {
+        self.cells.iter().all(|&cell| cell.is_some())
     }
 }
 
@@ -71,41 +83,63 @@ fn main() {
     loop {
         board.display();
 
-        println!("Player {}, enter your move (0-8):", current_player);
+        if current_player == Player::X {
+            println!("Player {}, enter your move (0-8):", current_player);
 
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read input.");
+            let mut input = String::new();
+            std::io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read input.");
 
-        let position: usize = match input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
+            let position: usize = match input.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Invalid input. Please enter a number from 0 to 8.");
+                    continue;
+                }
+            };
+
+            if position < 9 {
+                if let Some(_) = board.cells[position] {
+                    println!("Invalid move. The position is already occupied.");
+                    continue;
+                }
+
+                board.make_move(position, current_player);
+
+                if board.check_win(current_player) {
+                    println!("Player {} wins!", current_player);
+                    break;
+                }
+
+                if board.is_full() {
+                    println!("It's a tie!");
+                    break;
+                }
+
+                current_player = Player::O;
+            } else {
                 println!("Invalid input. Please enter a number from 0 to 8.");
                 continue;
             }
-        };
-
-        if position < 9 {
-            if let Some(_) = board.cells[position] {
-                println!("Invalid move. The position is already occupied.");
-                continue;
-            }
-
-            board.make_move(position, current_player);
+        } else {
+            println!("Computer's turn (Player {}).", current_player);
+            let available_moves = board.get_available_moves();
+            let random_index = rand::thread_rng().gen_range(0..available_moves.len());
+            let computer_move = available_moves[random_index];
+            board.make_move(computer_move, current_player);
 
             if board.check_win(current_player) {
                 println!("Player {} wins!", current_player);
                 break;
             }
 
-            current_player = match current_player {
-                Player::X => Player::O,
-                Player::O => Player::X,
-            };
-        } else {
-            println!("Invalid input. Please enter a number from 0 to 8.");
-            continue;
+            if board.is_full() {
+                println!("It's a tie!");
+                break;
+            }
+
+            current_player = Player::X;
         }
     }
 }
