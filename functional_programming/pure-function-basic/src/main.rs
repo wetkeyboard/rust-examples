7444e1a711657707baea69d7fc5387196e8cbff5
wetkeyboard17 <wetkeyboard17@gmail.com>
2023-08-05 12:47:06 +0100

    Test of functional programming - filter even numbers have been added.

--- functional_programming/pure-function-basic/src/main.rs
+++ functional_programming/pure-function-basic/src/main.rs
@@ -13,3 +13,31 @@ fn main() {
     println!("Original vector: {:?}", numbers);
     println!("Even numbers: {:?}", even_numbers);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_filter_even_numbers() {
+        // Test case 1: Even numbers in the vector
+        let input = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let expected_output = vec![2, 4, 6, 8, 10];
+        assert_eq!(filter_even_numbers(&input), expected_output);
+
+        // Test case 2: Only odd numbers in the vector
+        let input = vec![1, 3, 5, 7, 9];
+        let expected_output = Vec::<i32>::new(); // Empty vector as there are no even numbers
+        assert_eq!(filter_even_numbers(&input), expected_output);
+
+        // Test case 3: Only even numbers in the vector
+        let input = vec![2, 4, 6, 8];
+        let expected_output = vec![2, 4, 6, 8];
+        assert_eq!(filter_even_numbers(&input), expected_output);
+
+        // Test case 4: Empty vector
+        let input = Vec::<i32>::new();
+        let expected_output = Vec::<i32>::new();
+        assert_eq!(filter_even_numbers(&input), expected_output);
+    }
+}
